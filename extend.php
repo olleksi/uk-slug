27ccb8edaaecdb09f98003cd3144aac430a86a2a
olleksi <lieks.otaro@gmail.com>
2026-04-10 20:53:46 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -4,29 +4,22 @@ namespace Olleksi\UkSlug;
 
 use Flarum\Extend;
 use Flarum\Discussion\Event\Saving;
-use Illuminate\Support\Arr;
 
 return [
     (new Extend\Event)
         ->listen(Saving::class, function (Saving $event) {
             $discussion = $event->discussion;
             
-            // Перевірка: чи це об'єкт Discussion, а не інша модель
             if (!($discussion instanceof \Flarum\Discussion\Discussion)) {
                 return;
             }
             
-            $data = $event->data;
-            
-            // Перевіряємо, чи змінився заголовок обговорення
-            if (Arr::has($data, 'attributes.title')) {
-                $title = Arr::get($data, 'attributes.title');
+            if (isset($event->data['attributes']['title'])) {
+                $title = $event->data['attributes']['title'];
                 
                 if (!empty($title)) {
                     $discussion->slug = Transliterator::transliterate($title);
                 }
             }
         }),
-    
-    (new Extend\Locales(__DIR__ . '/locale')),
 ];
